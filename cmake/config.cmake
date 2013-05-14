4ad4607013cf56664d8ecf5733a77e9d84cbd98c
wnowak <nowak@locomotec.com>
2013-05-14 14:48:00 +0200

    forgot cmake file in last commit

---
+++ cmake/config.cmake
@@ -0,0 +1,15 @@
+#
+# Helper CMake file to define path to config files
+#
+#
+# Output:
+#    youbot_driver_CONFIG_DIR - Path to config files such as ethercat.cfg
+#
+
+set(youbot_driver_CONFIG_DIR ${youbot_driver_SOURCE_PREFIX}/config)
+
+message("lala geht")
+message("lala geht")
+message("lala geht")
+message(${youbot_driver_CONFIG_DIR})
+
